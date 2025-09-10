9038f2912755904221f105b519a38344cb9d3b23
Melly Dwiliani <mellydwiliani7@gmail.com>
2025-09-10 15:48:21 +0700

    feat: fixing background

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#000000',
+    theme_color: '#ff6ebc',
     icons: [
       {
         src: '/icon.png',
