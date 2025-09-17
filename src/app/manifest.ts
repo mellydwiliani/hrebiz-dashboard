a23e0a367c613c8cf19c1b288c14b150259aabe3
Melly Dwiliani <mellydwiliani7@gmail.com>
2025-09-17 12:00:12 +0700

    feat: add home screen

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#ff6ebc',
+    theme_color: '#ffffff',
     icons: [
       {
         src: '/icon.png',
